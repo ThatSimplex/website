3eaeb2441a8e1f0486d57aa62d6d67dbb984ff65
Simplex <jonaks13@gmail.com>
2020-10-04 20:23:17 +0200

    Added views and multiple pages

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-website
+This is just a project I use to learn Node.js, express and Pug.
+Don't expect anything spetacular.
--- app.js
+++ app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.set('view engine', 'pug');
+app.set('views', './src/views');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.listen(8000, () => {
     console.log("Server is running on port 8000");
 })
 
-app.get('/', (req, res) => {
-    res.send("Hello World!");
+app.route('/')
+.get((req, res) => {
+    res.render('index');
+})
+.post((req, res) => {
+    var txt = req.body.pwd;
+    console.log(txt);
+    res.send('It works!!');
+})
+
+app.route('/login')
+.get((req, res) => {
+    res.render('login');
+})
+.post((req, res) => {
+    var usr = req.body;
+    console.log(usr);
+    res.send('Username and Password works!');
 })
